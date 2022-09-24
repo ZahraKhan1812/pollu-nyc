dcfb94d777f5eb291fa1d34d14d291fb9cdf8fa6
Zahra Khan <zahrakhan@Zahras-MacBook-Air.local>
2022-09-24 13:36:45 -0400

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,371 @@
+import type { NextPage } from "next";
+import styles from "./index.module.css";
+
+const MacBookPro141: NextPage = () => {
+  return (
+    <div className={styles.macBookPro141}>
+      <div className={styles.rectangleDiv} />
+      <div className={styles.rectangleDiv1} />
+      <div className={styles.rectangleDiv2} />
+      <img className={styles.download1Icon} alt="" src="../download-1@2x.png" />
+      <div className={styles.letsReduceOurEmissions}>
+        Let’s reduce our emissions.
+      </div>
+      <div className={styles.lightLogoLeft5LinksCe}>
+        <div className={styles.headerBgDiv} />
+        <button className={styles.buttons}>
+          <div />
+          <div className={styles.containedDefault}>
+            <b className={styles.label}>Install Widget</b>
+          </div>
+        </button>
+        <div className={styles.linksDiv}>
+          <div className={styles.climatiQAPIDiv}>ClimatiQ API</div>
+          <div className={styles.climatiQAPIDiv}>Pollution Data</div>
+          <div className={styles.climatiQAPIDiv}>FAQ</div>
+          <div className={styles.itemFourDiv}>Item four</div>
+          <div className={styles.itemFourDiv}>Item five</div>
+        </div>
+        <div className={styles.companyLogo} />
+        <div className={styles.pollutedNYCDiv}>pollutedNYC</div>
+      </div>
+      <div className={styles.rectangleDiv3} />
+      <img className={styles.rectangleIcon} alt="" src="../rectangle-15.svg" />
+      <img className={styles.rectangleIcon1} alt="" src="../rectangle-41.svg" />
+      <div className={styles.rectangleDiv4} />
+      <div className={styles.rectangleDiv5} />
+      <img className={styles.unionIcon} alt="" />
+      <b className={styles.trackYourDailyCO2Footprint}>
+        <span className={styles.trackYourDailyContainer}>
+          <p className={styles.trackYourDaily}>{`Track your daily CO2 `}</p>
+          <p className={styles.trackYourDaily}>&nbsp;</p>
+          <p className={styles.trackYourDaily}>footprint and offset</p>
+          <p className={styles.trackYourDaily}>&nbsp;</p>
+          <p className={styles.trackYourDaily}>{`potential. `}</p>
+          <p className={styles.trackYourDaily}>&nbsp;</p>
+          <p className={styles.trackYourDaily}>&nbsp;</p>
+          <p className={styles.trackYourDaily}>{`Compare your `}</p>
+          <p className={styles.trackYourDaily}>&nbsp;</p>
+          <p className={styles.trackYourDaily}>{`emission levels to your `}</p>
+          <p className={styles.trackYourDaily}>&nbsp;</p>
+          <p className={styles.friendsP}>friends.</p>
+        </span>
+      </b>
+      <img className={styles.groupIcon} alt="" src="../group-12.svg" />
+      <b className={styles.pollutedNYCB}>pollutedNYC</b>
+      <div className={styles.sewageOverflowSingleUsePl}>
+        <p className={styles.trackYourDaily}>
+          Sewage overflow, single-use plastic,
+        </p>
+        <p className={styles.trackYourDaily}>&nbsp;</p>
+        <p
+          className={styles.trackYourDaily}
+        >{`and toxic gas emissions from `}</p>
+        <p className={styles.trackYourDaily}>&nbsp;</p>
+        <p
+          className={styles.trackYourDaily}
+        >{`transportation are leading causes of `}</p>
+        <p className={styles.trackYourDaily}>&nbsp;</p>
+        <p className={styles.trackYourDaily}>
+          resource pollution in New York City.
+        </p>
+        <p className={styles.friendsP}>{` `}</p>
+      </div>
+      <div className={styles.trackNYCsEmissionLevels}>
+        <span className={styles.trackYourDailyContainer}>
+          <p className={styles.trackYourDaily}>Track NYC’s</p>
+          <p className={styles.trackYourDaily}>&nbsp;</p>
+          <p className={styles.trackYourDaily}>&nbsp;</p>
+          <p className={styles.trackYourDaily}>&nbsp;</p>
+          <p className={styles.friendsP}>{` Emission Levels `}</p>
+        </span>
+      </div>
+      <div className={styles.currentNYCPollutionData}>
+        Current NYC Pollution Data
+      </div>
+      <div className={styles.barGraphDiv}>
+        <div className={styles.rectangleDiv6} />
+        <div className={styles.rectangleDiv7} />
+        <textarea className={styles.rectangleTextarea} />
+        <div className={styles.rectangleDiv8} />
+        <div className={styles.rectangleDiv9} />
+        <div className={styles.rectangleDiv10} />
+      </div>
+      <div className={styles.dataTableDiv}>
+        <div className={styles.groupDiv}>
+          <div className={styles.dataTableDiv1}>
+            <div className={styles.rectangleDiv11} />
+            <div className={styles.tableDiv}>
+              <div className={styles.groupDiv1}>
+                <div className={styles.rectangleDiv12} />
+              </div>
+              <div className={styles.rectangleDiv13} />
+              <div className={styles.groupDiv2}>
+                <div className={styles.rectangleDiv14} />
+                <div className={styles.groupDiv3}>
+                  <div className={styles.rectangleDiv14} />
+                </div>
+                <div className={styles.groupDiv4}>
+                  <div className={styles.rectangleDiv14} />
+                </div>
+                <div className={styles.groupDiv5}>
+                  <div className={styles.rectangleDiv12} />
+                </div>
+                <div className={styles.groupDiv6}>
+                  <div className={styles.rectangleDiv12} />
+                </div>
+              </div>
+              <div className={styles.groupDiv3}>
+                <div className={styles.rectangleDiv12} />
+                <div className={styles.groupDiv3}>
+                  <div className={styles.rectangleDiv12} />
+                </div>
+              </div>
+            </div>
+            <div className={styles.emissionDiv}>Emission</div>
+            <div className={styles.rectangleDiv21} />
+          </div>
+        </div>
+      </div>
+      <div className={styles.subscribeBarDiv}>
+        <div className={styles.rectangleDiv22} />
+        <b
+          className={styles.subscribeGetUpdates}
+        >{`Subscribe & Get Updates`}</b>
+      </div>
+      <img
+        className={styles.imageRemovebgPreview41}
+        alt=""
+        src="../imageremovebgpreview-4-1@2x.png"
+      />
+      <div className={styles.usingClimatiqAPICalculateE}>
+        <span className={styles.trackYourDailyContainer}>
+          <p className={styles.trackYourDaily}>
+            <span>Using Climatiq API</span>
+          </p>
+          <p className={styles.blankLineP12}>
+            <span>&nbsp;</span>
+          </p>
+          <p className={styles.friendsP}>
+            <span>Calculate emissions in real-time</span>
+          </p>
+        </span>
+      </div>
+      <div className={styles.outlinedDefault}>
+        <b className={styles.label1}>Learn more</b>
+      </div>
+      <img
+        className={styles.howToUseWidgetsRemovebgPrIcon}
+        alt=""
+        src="../howtousewidgetsremovebgpreview-1@2x.png"
+      />
+      <div className={styles.rectangleDiv23} />
+      <div className={styles.rectangleDiv24} />
+      <div className={styles.rectangleDiv25} />
+      <div className={styles.rectangleDiv26} />
+      <div className={styles.offsetDiv}>Offset</div>
+      <div className={styles.emissionsDiv}>Emissions</div>
+      <div className={styles.michaelPDiv}>{`Michael P. `}</div>
+      <div className={styles.lbsDiv}>4.5 lbs</div>
+      <div className={styles.lbsDiv1}>4.5 lbs</div>
+      <div className={styles.rectangleDiv27} />
+      <div className={styles.lbsDiv2}>2.7 lbs</div>
+      <div className={styles.rectangleDiv28} />
+      <div className={styles.nicholeRMe}>{`Nichole R. (me) `}</div>
+      <div className={styles.lbsDiv3}>2.1 lbs</div>
+      <div className={styles.rectangleDiv29} />
+      <div className={styles.saraADiv}>Sara A.</div>
+      <div className={styles.lbsDiv4}>1.9 lbs</div>
+      <div className={styles.cO2EmissionLeaderboard}>
+        CO2 Emission Leaderboard
+      </div>
+      <div className={styles.dateDiv}>Date</div>
+      <div className={styles.sectorDiv}>Sector</div>
+      <div className={styles.emissionDiv1}>Emission</div>
+      <div className={styles.categoryDiv}>Category</div>
+      <div className={styles.categoryDiv1}>Category</div>
+      <div className={styles.ctorDiv}>ctor</div>
+      <div className={styles.cO2e0189012KgkWhCO201}>
+        <span className={styles.trackYourDailyContainer}>
+          <p className={styles.trackYourDaily}>CO2e: 0.189012 kg/kWh</p>
+          <p className={styles.trackYourDaily}>CO2: 0.188223 kg/kWh</p>
+          <p className={styles.trackYourDaily}>CH4: 0.000013 kg/kWh</p>
+          <p className={styles.friendsP}>N2O: 0.000002 kg/kWh</p>
+        </span>
+      </div>
+      <div className={styles.energyDiv}>Energy</div>
+      <div className={styles.electricityDiv}>Electricity</div>
+      <div className={styles.div}>2022</div>
+      <div className={styles.cO2e0189012KgkWhCO201}>
+        <span className={styles.trackYourDailyContainer}>
+          <p className={styles.trackYourDaily}>CO2e: 0.189012 kg/kWh</p>
+          <p className={styles.trackYourDaily}>CO2: 0.188223 kg/kWh</p>
+          <p className={styles.trackYourDaily}>CH4: 0.000013 kg/kWh</p>
+          <p className={styles.friendsP}>N2O: 0.000002 kg/kWh</p>
+        </span>
+      </div>
+      <div className={styles.energyDiv}>Energy</div>
+      <div className={styles.div}>2022</div>
+      <div className={styles.cO2e0189012KgkWhCO201}>
+        <span className={styles.trackYourDailyContainer}>
+          <p className={styles.trackYourDaily}>CO2e: 0.189012 kg/kWh</p>
+          <p className={styles.trackYourDaily}>CO2: 0.188223 kg/kWh</p>
+          <p className={styles.trackYourDaily}>CH4: 0.000013 kg/kWh</p>
+          <p className={styles.friendsP}>N2O: 0.000002 kg/kWh</p>
+        </span>
+      </div>
+      <div className={styles.energyDiv}>Energy</div>
+      <div className={styles.div}>2022</div>
+      <div className={styles.electricityDiv1}>Electricity</div>
+      <div className={styles.cO2e0189012KgkWhCO2013}>
+        <span className={styles.trackYourDailyContainer}>
+          <p className={styles.trackYourDaily}>CO2e: 0.189012 kg/kWh</p>
+          <p className={styles.trackYourDaily}>CO2: 0.188223 kg/kWh</p>
+          <p className={styles.trackYourDaily}>CH4: 0.000013 kg/kWh</p>
+          <p className={styles.friendsP}>N2O: 0.000002 kg/kWh</p>
+        </span>
+      </div>
+      <div className={styles.energyDiv3}>Energy</div>
+      <div className={styles.div3}>2022</div>
+      <div className={styles.electricityDiv2}>Electricity</div>
+      <div className={styles.cO2e0189012KgkWhCO2014}>
+        <span className={styles.trackYourDailyContainer}>
+          <p className={styles.trackYourDaily}>CO2e: 0.189012 kg/kWh</p>
+          <p className={styles.trackYourDaily}>CO2: 0.188223 kg/kWh</p>
+          <p className={styles.trackYourDaily}>CH4: 0.000013 kg/kWh</p>
+          <p className={styles.friendsP}>N2O: 0.000002 kg/kWh</p>
+        </span>
+      </div>
+      <div className={styles.energyDiv4}>Energy</div>
+      <div className={styles.div4}>2022</div>
+      <div className={styles.electricityDiv3}>Electricity</div>
+      <div className={styles.cO2e0189012KgkWhCO2015}>
+        <span className={styles.trackYourDailyContainer}>
+          <p className={styles.trackYourDaily}>CO2e: 0.189012 kg/kWh</p>
+          <p className={styles.trackYourDaily}>CO2: 0.188223 kg/kWh</p>
+          <p className={styles.trackYourDaily}>CH4: 0.000013 kg/kWh</p>
+          <p className={styles.friendsP}>N2O: 0.000002 kg/kWh</p>
+        </span>
+      </div>
+      <div className={styles.energyDiv5}>Energy</div>
+      <div className={styles.div5}>2022</div>
+      <div className={styles.electricityDiv4}>Electricity</div>
+      <div className={styles.cO2e0189012KgkWhCO2016}>
+        <span className={styles.trackYourDailyContainer}>
+          <p className={styles.trackYourDaily}>CO2e: 0.189012 kg/kWh</p>
+          <p className={styles.trackYourDaily}>CO2: 0.188223 kg/kWh</p>
+          <p className={styles.trackYourDaily}>CH4: 0.000013 kg/kWh</p>
+          <p className={styles.friendsP}>N2O: 0.000002 kg/kWh</p>
+        </span>
+      </div>
+      <div className={styles.energyDiv6}>Energy</div>
+      <div className={styles.div6}>2022</div>
+      <div className={styles.electricityDiv5}>Electricity</div>
+      <div className={styles.cO2e0189012KgkWhCO2017}>
+        <span className={styles.trackYourDailyContainer}>
+          <p className={styles.trackYourDaily}>CO2e: 0.189012 kg/kWh</p>
+          <p className={styles.trackYourDaily}>CO2: 0.188223 kg/kWh</p>
+          <p className={styles.trackYourDaily}>CH4: 0.000013 kg/kWh</p>
+          <p className={styles.friendsP}>N2O: 0.000002 kg/kWh</p>
+        </span>
+      </div>
+      <div className={styles.energyDiv7}>Energy</div>
+      <div className={styles.div7}>2022</div>
+      <div className={styles.electricityDiv6}>Electricity</div>
+      <div className={styles.cO2e0189012KgkWhCO2018}>
+        <span className={styles.trackYourDailyContainer}>
+          <p className={styles.trackYourDaily}>CO2e: 0.189012 kg/kWh</p>
+          <p className={styles.trackYourDaily}>CO2: 0.188223 kg/kWh</p>
+          <p className={styles.trackYourDaily}>CH4: 0.000013 kg/kWh</p>
+          <p className={styles.friendsP}>N2O: 0.000002 kg/kWh</p>
+        </span>
+      </div>
+      <div className={styles.energyDiv8}>Energy</div>
+      <div className={styles.div8}>2022</div>
+      <img className={styles.rectangleIcon2} alt="" />
+      <div className={styles.rectangleDiv30} />
+      <div className={styles.q1Div}>
+        <div className={styles.breakDownEmissionsAndOffse}>
+          Break down emissions and offset.
+        </div>
+        <div className={styles.q1Div1}>
+          <img
+            className={styles.rectangleIcon3}
+            alt=""
+            src="../rectangle-26.svg"
+          />
+          <img className={styles.vectorIcon} alt="" src="../vector.svg" />
+          <div className={styles.whatIsPollutedNYC}>What is pollutedNYC?</div>
+        </div>
+      </div>
+      <div className={styles.q2Div}>
+        <div className={styles.groupDiv9}>
+          <div className={styles.rectangleDiv31} />
+          <img className={styles.vectorIcon1} alt="" src="../vector1.svg" />
+        </div>
+        <div className={styles.whatsPollutedNYCsGoal}>
+          What's pollutedNYC’s goal?
+        </div>
+      </div>
+      <div className={styles.groupDiv10}>
+        <div className={styles.rectangleDiv31} />
+        <img className={styles.vectorIcon1} alt="" src="../vector1.svg" />
+      </div>
+      <div className={styles.howCanPollutedNYCHelpTheC}>
+        How can pollutedNYC help the community?
+      </div>
+      <div className={styles.groupDiv11}>
+        <div className={styles.rectangleDiv31} />
+        <div className={styles.whatArePollutedNYCsNextSt}>
+          What are pollutedNYC’s next step?
+        </div>
+        <img className={styles.vectorIcon3} alt="" src="../vector1.svg" />
+      </div>
+      <div className={styles.groupDiv12}>
+        <div className={styles.rectangleDiv31} />
+        <img className={styles.vectorIcon1} alt="" src="../vector1.svg" />
+      </div>
+      <div className={styles.iHaveAnotherQuestion}>
+        I have another question...
+      </div>
+      <b className={styles.fAQB}>FAQ</b>
+      <div className={styles.followUsOn}> Follow us on</div>
+      <img className={styles.vectorIcon5} alt="" src="../vector5.svg" />
+      <img className={styles.vectorIcon6} alt="" src="../vector6.svg" />
+      <img className={styles.vectorIcon7} alt="" src="../vector7.svg" />
+      <div className={styles.lineDiv} />
+      <b className={styles.installWidgetB}>Install Widget</b>
+      <div className={styles.termsDiv}>Terms</div>
+      <div className={styles.contactUsDiv}>Contact Us</div>
+      <div className={styles.privacyDiv}>Privacy</div>
+      <div className={styles.securityDiv}>
+        <span className={styles.trackYourDailyContainer}>
+          <p className={styles.trackYourDaily}>Security</p>
+        </span>
+      </div>
+      <div className={styles.nBchartsRadarDiv}>
+        <img className={styles.backgroundIcon} alt="" src="../background.svg" />
+        <img className={styles.radarDataIcon} alt="" src="../radar-data.svg" />
+        <img className={styles.pointsIcon} alt="" src="../points.svg" />
+        <div className={styles.textDiv}>
+          <div className={styles.photoshopDiv}>Photoshop</div>
+          <div className={styles.illustratorDiv}>Illustrator</div>
+          <div className={styles.xdDiv}>Xd</div>
+          <div className={styles.indesignDiv}>Indesign</div>
+          <div className={styles.premiereDiv}>Premiere</div>
+        </div>
+      </div>
+      <div className={styles.q1Div2}>
+        <img
+          className={styles.rectangleIcon4}
+          alt=""
+          src="../rectangle-261.svg"
+        />
+        <b className={styles.subscribe}>Subscribe</b>
+      </div>
+      <img className={styles.q1Icon} alt="" src="../q1.svg" />
+    </div>
+  );
+};
+
+export default MacBookPro141;
